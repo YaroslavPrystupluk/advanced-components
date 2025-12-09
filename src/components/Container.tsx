4c634de61d4126818a76fd9071af35a9a6583397
Yaroslav Prystupliuk <106585260+YaroslavPrystupluk@users.noreply.github.com>
2025-12-09 20:19:41 +0200

    Fix Container component type definitions

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -3,8 +3,8 @@ import type {ComponentPropsWithoutRef, ComponentType, ElementType, FC, ReactNode
 type ContainerProps<T extends ElementType> = {
     as?: T;
     children: ReactNode;
-} & ComponentPropsWithoutRef<ComponentType>
-const Container: FC<ContainerProps<ElementType>> = <C extends ElementType>(
+} & ComponentPropsWithoutRef<T>
+const Container = <C extends ElementType>(
     {as, children, ...props}: ContainerProps<C>
 ) => {
     let Component = as || 'div';
